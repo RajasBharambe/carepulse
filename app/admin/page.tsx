c7f6d8ff1ef0c23e63d96c413cbb8122391e8aff
Rajas Bharambe <89535670+RajasBharambe@users.noreply.github.com>
2025-02-01 18:39:40 +0530

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,7 @@
 import { StatCard } from "@/components/StatCard";
 import { columns } from "@/components/table/columns";
 import { DataTable } from "@/components/table/DataTable";
-import { getRecentAppointmentList } from "@/lib/actions/appointment.actions";
+import { getRecentAppointmentList } from "@/lib/actions/appointment.actions"; 
 import Image from "next/image";
 import Link from "next/link";
 
